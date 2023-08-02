7b6979b866f04f45d3e144d8d9fcd40c67317795
CallMeYurii <vad4807@gmail.com>
2023-08-02 13:37:25 +0300

    Finish UI for spends and categories
    For spends note input field was added. Also the issue with sending reques to the server was fixed. At categories page style for "add button" was updated.

--- FinancialGoals.API/FinancialGoals.Core/DTOs/Transaction/TransactionToCreate.cs
+++ FinancialGoals.API/FinancialGoals.Core/DTOs/Transaction/TransactionToCreate.cs
@@ -7,7 +7,7 @@ public class TransactionToCreate
     public double Amount { get; set; }
     public TransactionType Type { get; set; }
     public DateTime Date { get; set; }
-    public string Description { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
     public int FinancialAccountId { get; set; }
     public int CategoryId { get; set; }
 }
